ad04bd827330f2d842741a3e693e34531c67de40
ihsavru <vurvashi11@gmail.com>
2023-05-31 00:46:04 +0530

    Add download image button

--- src/components/imageCanvas.js
+++ src/components/imageCanvas.js
@@ -11,9 +11,24 @@ const StyledDiv = styled.div`
 `;
 
 const ImageCanvas = ({ word, font, canvasRef }) => {
+  const downloadImage = () => {
+    const canvas = canvasRef.current;
+    const dataURL = canvas.toDataURL();
+    const downloadLink = document.createElement("a");
+    downloadLink.href = dataURL;
+    downloadLink.download = `${word}.png`;
+    downloadLink.click();
+  };
+
   return (
-    <div className='text-center mt-20'>
+    <div className='text-center mt-20 relative'>
       <StyledDiv className='w-full flex h-full rounded bg-white border border-gray-300'>
+        <button
+          className='absolute top-5 right-5 rounded bg-cyan-700 font-bold uppercase text-sm px-3 py-2 text-white'
+          onClick={downloadImage}
+        >
+          Download Image
+        </button>
         <StyledCanvas ref={canvasRef} font={font}>
           <span className='self-center'>{word}</span>
         </StyledCanvas>
